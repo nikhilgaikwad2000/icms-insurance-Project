f7abb83ee60cf573b96ab64ce9e91dd26e95f14f
nikhil2000 <nikhilgaikwad6784@gmail.com>
2025-08-03 22:15:01 +0530

    Remove the boiler code add @Data Annotation

--- src/main/java/com/icms/insurance/entity/User.java
+++ src/main/java/com/icms/insurance/entity/User.java
@@ -30,33 +30,6 @@ public class User {
 
     private Double income;
 
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
 
     public void setRole(String role) {
         this.role = role;
