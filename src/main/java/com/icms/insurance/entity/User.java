7cbac4c1cf61b33eee30fbc972eb06245f745ffb
nikhil2000 <nikhilgaikwad6784@gmail.com>
2025-08-03 22:15:30 +0530

    Remove the boiler code add @Data Annotation

--- src/main/java/com/icms/insurance/entity/User.java
+++ src/main/java/com/icms/insurance/entity/User.java
@@ -31,33 +31,6 @@ public class User {
     private Double income;
 
 
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "createDate=" + createDate +
-                ", userId=" + userId +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", role='" + role + '\'' +
-                ", status='" + status + '\'' +
-                '}';
-    }
-
-    public User() {
-    }
 
 
 }
