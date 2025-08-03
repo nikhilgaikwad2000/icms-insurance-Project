370ae7626d1b9076df14b049a0fa0fc0cb0de6f8
nikhil2000 <nikhilgaikwad6784@gmail.com>
2025-08-03 22:43:56 +0530

    Remove Getter and Setter Add The @Data Lombok.

--- src/main/java/com/icms/insurance/entity/User.java
+++ src/main/java/com/icms/insurance/entity/User.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class User {
     @Id
     @GeneratedValue
